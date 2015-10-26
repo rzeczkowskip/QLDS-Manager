8ba39c5459c359557840286900bd47ee82c7a30c
Thomas Jones <thomas@tomtecsolutions.com>
2015-10-26 20:51:03 +1000

    Update check_wget.sh

--- src/check_wget.sh
+++ src/check_wget.sh
@@ -1,6 +1,9 @@
-if [ ! -x "$(command -v wget)" ]; then
-  return 1
-else
-  echo "\"wget\" is not installed. Please install \"wget\" and re-run this script."
-  exit 1
-fi
+#!/bin/bash
+check_wget () {
+  if [ ! -x "$(command -v wget)" ]; then
+    return 1
+  else
+    echo "\"wget\" is not installed. Please install \"wget\" and re-run this script."
+    exit 1
+  fi
+}
