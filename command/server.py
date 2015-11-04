362b2f25ecac98a81c478876a808cd1126aed420
Piotr Rzeczkowski <piotr@rzeka.net>
2015-11-04 22:55:50 +0100

    Add server subcommands

--- command/server.py
+++ command/server.py
@@ -1,6 +1,7 @@
 from command.default import ManagerDefaultController
 from cement.core.controller import expose
-from util.config import ServerConfig
+from util.config import Configuration, ServerConfig
+from util.supervisor import Supervisor
 
 
 class ServerController(ManagerDefaultController):
@@ -9,6 +10,11 @@ class ServerController(ManagerDefaultController):
         description = 'Server management'
         arguments = [
             (['--check'], dict(help='Checks configuration and exits app', action='store_true')),
+            (['server_ids'], dict(
+                help='Server id available in servers config file (servers:<id>:group)',
+                action='store',
+                nargs='*'
+            ))
         ]
 
     @expose(hide=True)
@@ -18,6 +24,47 @@ class ServerController(ManagerDefaultController):
 
         self.app.args.parse_args(['--help'])
 
-    def check(self):
-        ServerConfig()
-        exit()
+    @expose(help='Start server <server_id>')
+    def start(self):
+        supervisor = Supervisor()
+        server_config = ServerConfig()
+
+        for sid in self.app.pargs.server_ids:
+            if sid in server_config.servers:
+                supervisor.ctl(['start', supervisor.process_prefix + sid])
+            else:
+                print('Server %s doesn\'t exists' % str(sid))
+
+    @expose(help='Stop server <server_id>')
+    def stop(self):
+        supervisor = Supervisor()
+        server_config = ServerConfig()
+
+        for sid in self.app.pargs.server_ids:
+            if sid in server_config.servers:
+                supervisor.ctl(['stop', supervisor.process_prefix + sid])
+            else:
+                print('Server %s doesn\'t exists' % str(sid))
+
+    @expose(help='Restart server <server_id>')
+    def restart(self):
+        supervisor = Supervisor()
+        server_config = ServerConfig()
+
+        for sid in self.app.pargs.server_ids:
+            if sid in server_config.servers:
+                supervisor.ctl(['restart', supervisor.process_prefix + sid])
+            else:
+                print('Server %s doesn\'t exists' % str(sid))
+
+    @expose(help='Attch server <server_id>')
+    def attach(self):
+        supervisor = Supervisor()
+        server_config = ServerConfig()
+
+        for sid in self.app.pargs.server_ids:
+            if sid in server_config.servers:
+                supervisor.ctl(['fg', supervisor.process_prefix + sid])
+            else:
+                print('Server %s doesn\'t exists' % str(sid))
+
