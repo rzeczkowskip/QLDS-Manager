ed4028f7c80f89405d11984934a3235a276201d9
Piotr Rzeczkowski <piotr@rzeka.net>
2015-11-17 16:03:09 +0100

    Add signal handling

--- manager.py
+++ manager.py
@@ -1,5 +1,7 @@
 from cement.core.foundation import CementApp
 import command
+import signal
+from cement.core.exc import CaughtSignal
 
 
 class Manager(CementApp):
@@ -9,4 +11,9 @@ class Manager(CementApp):
 
 
 with Manager() as app:
-    app.run()
+    try:
+        app.run()
+    except CaughtSignal as e:
+        # do something with e.signum or e.frame (passed from signal)
+        if e.signum in [signal.SIGTERM, signal.SIGINT]:
+            print('Exiting...')
